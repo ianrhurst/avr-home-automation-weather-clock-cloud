c0a61d8a530e365a65a268216f9feb6c3907de81
ianrhurst <64507525+ianrhurst@users.noreply.github.com>
2020-04-29 12:53:42 -0400

    Update config.py

--- config.py
+++ config.py
@@ -20,15 +20,15 @@
 
 # GCloud specific config
 PROJECT_ID = "iot-weather-clock"
-IOT_CORE_REGION = "europe-west1"
+IOT_CORE_REGION = "us-central1"
 IOT_CORE_REGISTRY_ID = "weather-devices"
-IOT_CORE_DEVICE_ID = "d0123710B94CEB0ECFE"
+IOT_CORE_DEVICE_ID = "d0123f77e05e73abafe"
 
 # Location to fetch data from. See yr.no for more information
 YR_LOCATION_URL = "https://www.yr.no/place/Norway/Tr%C3%B8ndelag/Trondheim/Trondheim/forecast.xml"
 
 # The cloud storage bucket to store the cached data. See https://console.cloud.google.com/storage for more information
-CLOUD_STORAGE_BUCKET_ID = "weather-clock-cache"
+CLOUD_STORAGE_BUCKET_ID = "weather-clock-cache1"
 
 # Temperature range for the clock in celsius.
 TEMP_MAX = 15
